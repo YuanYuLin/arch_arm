38babefd7e594ef52a6041f7cbf7616623b096a3
yuanyu.lin <yuanyu.lin@gmail.com>
2017-07-11 16:49:56 +0800

    MAJOR_NUM=1 MINOR_NUM=2 AUX_NUM=0 update package python script

--- Package/CONFIG.py
+++ Package/CONFIG.py
@@ -1,40 +1,62 @@
 import ops
+import iopc
 
-def MAIN_ENV(args):
+pkg_path = ""
+output_dir = ""
+
+def set_global(args):
+    global pkg_path 
+    global output_dir
     pkg_path = args["pkg_path"]
     output_dir = args["output_path"]
 
+def MAIN_ENV(args):
+    set_global(args)
+    print ops.getEnv("ARCH_ALT")
+    print ops.getEnv("DO_DEBOOTSTRAP")
+
     ops.exportEnv(ops.setEnv("ARCH", "arm"))
-    ops.exportEnv(ops.setEnv("ARCH_ALT", "armhf"))
+    #ops.exportEnv(ops.setEnv("ARCH_ALT", "armhf"))
     ops.exportEnv(ops.setEnv("SDKSTAGE", output_dir + "/sdkstage"))
     ops.exportEnv(ops.setEnv("ARCH_ROOTFS", output_dir + "/rootfs"))
     ops.exportEnv(ops.setEnv("PACKAGES_DIR", output_dir + "/packages_dir"))
     ops.exportEnv(ops.setEnv("BASE_ROOTFS_DIR", output_dir))
+    ops.exportEnv(ops.setEnv("OUTPUT_ROOTFS_DIR", output_dir))
     return False
 
 def MAIN_EXTRACT(args):
-    output_dir = args["output_path"]
+    set_global(args)
     ops.mkdir(ops.getEnv("SDKSTAGE"))
     ops.mkdir(ops.getEnv("ARCH_ROOTFS"))
     ops.mkdir(ops.getEnv("PACKAGES_DIR"))
-    return False
+    return True
+
+def MAIN_PATCH(args, patch_group_name):
+    set_global(args)
+    for patch in iopc.get_patch_list(pkg_path, patch_group_name):
+        if iopc.apply_patch(output_dir, patch):
+            continue
+        else:
+            sys.exit(1)
+
+    return True
 
 def MAIN_CONFIGURE(args):
-    output_dir = args["output_path"]
+    set_global(args)
     return False
 
 def MAIN_BUILD(args):
-    output_dir = args["output_path"]
+    set_global(args)
     return False
 
 def MAIN_INSTALL(args):
-    output_dir = args["output_path"]
+    set_global(args)
     return False
 
 def MAIN_CLEAN_BUILD(args):
-    output_dir = args["output_path"]
+    set_global(args)
     return False
 
 def MAIN(args):
-    output_dir = args["output_path"]
+    set_global(args)
 
